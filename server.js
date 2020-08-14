8185dd04195d0393321944efd4610ff4cc9fcf92
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-08-13 23:09:56 -0400

    Deploy Troubleshooting * Moved dotenv in dev dependency * cleaned up start script * Verfied PORT setting

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const postsRouter = require('./posts/postRouter');
 const usersRouter = require('./users/userRouter');
+const server = express();
+
 const logger = require('./middleware/logger');
 
-const server = express();
-server.use(express.json({}))
+server.use(express.json());
 server.use('/api/users', usersRouter);
 server.use('/api/posts', postsRouter);
 
 server.use((err, req, res, next) => {
   console.log(err);
-	res.status(500).json({
+	return res.status(500).json({
 		message: "Something went wrong, please try again later",
 	});
 });
@@ -21,8 +22,5 @@ server.use(logger());
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-
-
 
 module.exports = server;
